1dedac71e296095c6082ffaff8246b702a98ef14
99003549 <78538040+99003549@users.noreply.github.com>
2021-03-31 09:21:54 +0530

    Update Day3TextEnter.java

--- Day3/Day3TextEnter.java
+++ Day3/Day3TextEnter.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Day3TextEnter {
@@ -10,10 +11,19 @@ public class Day3TextEnter {
 		
 		driver.get("http://demo.automationtesting.in/Frames.html");
 		
-		driver.switchTo().frame("singleframe");
+		//driver.switchTo().frame("singleframe");
+		//driver.findElement(By.xpath("/html/body/section/div/div/div/input")).sendKeys("Rehana");
+		
+		//driver.findElement(By.className("analystic")).click();
+		
+
+		driver.findElement(By.xpath("//section/div/div/div/div/div/div/ul/li[2]/a[@class=\"analystic\"]")).click();
+		
+		WebElement ele= driver.findElement(By.xpath("/html/body/section/div[1]/div/div/div/div[2]/div[2]/iframe"));
+		driver.switchTo().frame(ele);
+		WebElement ele1= driver.findElement(By.xpath("/html/body/section/div/div/iframe"));
+		driver.switchTo().frame(ele1);
 		driver.findElement(By.xpath("/html/body/section/div/div/div/input")).sendKeys("Rehana");
-		//driver.switchTo().frame("Iframe with in an Iframe");
-		driver.findElement(By.className("analystic")).click();
 	}
 
 }
